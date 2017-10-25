8201242def8e745e8ad0121dc06e3e359d885d3f
Ryan Van Spengen <r.vanspengen@shaw.ca>
2017-10-25 00:01:25 +0000

    Implement Basic Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :users
   
   get 'users/new'
@@ -9,5 +11,8 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get  '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
 
