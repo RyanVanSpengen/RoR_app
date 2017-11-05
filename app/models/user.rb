4d9463280dfc41da103f1645ddfe9a70f1245e51
Ryan Van Spengen <r.vanspengen@shaw.ca>
2017-11-05 18:55:44 +0000

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,17 @@ class User < ApplicationRecord
   
   has_many :microposts, dependent: :destroy 
   
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
   
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+                                  
+                                
   
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -67,7 +77,25 @@ class User < ApplicationRecord
   end
     
   def feed
-    Micropost.where("user_id= ?", id)
+    following_ids = "SELECT followed_id FROM Relationships 
+                    WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                    OR user_id = :user_id", user_id: id)
+  end
+  
+  #Follows a user
+  def follow(other_user)
+    following << other_user
+  end
+  
+  #Unfollow a user
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+  
+  #Returns true if the user is following the other user
+  def following?(other_user)
+    following.include?(other_user)
   end
   
   private
